801df4a7ce791db78777d37918fb50ad2c7d8cd9
KOITT <KOITT@KOITT-PC>
2018-05-21 21:31:58 +0900

    안녕 나느 조서인이야

--- ch01/src/ch01/Hello.java
+++ ch01/src/ch01/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Hello, World.");
+        System.out.println("안녕, 세계.");
+        System.out.println("나는 조서인이야");
+        
+      
+        
 	}
 
 }
